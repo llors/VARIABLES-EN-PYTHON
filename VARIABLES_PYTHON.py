1c37fc7a906eaf802994ce6103d91dc2f18fa80a
llors <57237720+llors@users.noreply.github.com>
2020-01-08 17:24:49 -0500

    Add files via upload

---
+++ VARIABLES_PYTHON.py
@@ -0,0 +1,259 @@
+#    VARIABLES EN PYTHON
+
+nombre="GINO"
+
+print("el nombre es:", nombre)
+
+universidad="UNPRG"
+
+print("LA universidad es:", universidad)
+
+uni="IINGENERIA_ELECTRONICA"
+
+print("el valor uni es:" , uni)
+
+print("HOLA MI NOMBRE ES " +  nombre + " y estudio en la universidad " +  universidad + " y estudio" + uni)
+
+edad="20"
+
+print("el valor de edad es:" + edad)
+
+mL="mañana"
+
+print("el valor de mnn es:" + mL)
+
+eda="21"
+
+print("el valor de eda es:" + eda)
+
+print("mi edad es " + edad + " pero " + mL + " cumpliré los " + eda)
+
+facultad="FACFYM"
+
+print("el valor de facultad es:" + facultad)
+
+escuela="Ing.Electronica"
+
+print("el valor de escuela es:" + escuela)
+
+print("pertenezco a la facultad " + facultad + " de la escuela profesional de " + escuela)
+
+dia="11"
+
+print("el valor de dia es:" + dia)
+
+mes="noviembre"
+
+print("el valor de mes es:" + mes)
+
+anio="1996"
+
+print("el valor de anio es:" + anio)
+
+print("mi fecha de nacimiento fue el " + dia + " del mes de " + mes + " del " + anio)
+
+h="hermano"
+
+print("el hmn es:" + h)
+
+pa="papa"
+
+print("papa es:" + pa)
+
+ama="mama"
+
+print("el valor de ama es:" + ama)
+
+print("yo vivo con mi " + h + " y gracias a Dios con mi " + pa + " y mi " + ama + "tambien")
+
+prog="programacion"
+
+print("el valor de prog es:" + prog)
+
+ciclo="2019-II"
+
+print("el valor de ciclo es:" + ciclo)
+
+print("llevo el curso de " + prog  + " en el ciclo " + ciclo)
+
+sel="seleccion"
+
+print("el valor de selec es:" + sel)
+
+pais="peru"
+
+print("el valor de pais es" + pais)
+
+pais2="rusia"
+
+print("el valor de pais2 es" + pais2)
+
+print("la " + sel + " de " + pais + " fue al mundial de " + pais2)
+
+i="ingrese"
+
+print(" ingre es:" + i)
+
+admi="admision"
+
+print("admi es:" + admi)
+
+ordi="ordinario"
+
+print("ordi es:" + ordi)
+
+print("yo " + i + " en el examen de " + admi + " de modalidad " + ordi)
+
+linc="licenciamiento"
+
+print(" licencia es:" + linc)
+
+sune="SUNEDU"
+
+print("sunedu es:" + sune)
+
+print("el " + linc + "de la " + universidad + " lo dara la " + sune)
+
+fiel="fidelidad"
+
+print("el valor de fiel es:" + fiel)
+
+sentimiento="sentimental"
+
+print("el valor de sentim es:" + sentimiento)
+
+duda="dudas"
+
+print("el valor de duda es:" + duda)
+
+print("la " + fiel + " en una relacion " + sentimiento + " nunca generara " + duda)
+
+provincia="lambayeque"
+
+print("la provincia es:" + provincia)
+
+distrito="morrope"
+print("el distrito es:" + distrito)
+
+departamento="Lambayeque"
+
+print("el departamento es:" + departamento)
+
+materno="valdera"
+
+print("el apellido materno es:" + materno)
+
+sexo="masculino"
+
+print("el sexo es:" + sexo)
+
+hospital="essalud"
+
+print("el valor de hospital es:" + hospital)
+
+importe="180"
+
+print("el valor de importe es:" + importe)
+
+celular="93047656"
+
+print("el valor de numero de celular es:" + celular)
+
+codigo="199132d"
+
+print("el valor de codigo modular es:" + codigo)
+
+nota="18"
+
+print("la nota del alumno es:" + nota)
+
+grados_estudio="terminado"
+
+print("el valor de estudio es:" + grados_estudio)
+
+anio="11-11-2000"
+
+print("el valor de fecha de expedicion es" + anio)
+
+matematica="20"
+
+print("el valor de nota de matematica es:" + matematica)
+
+nota_ingles="16"
+
+print("la nota de ingles es:" + nota_ingles)
+
+nota_historia="16"
+
+print("la nota de historia es:" + nota_historia)
+
+geografia="16"
+
+print("la nota de geografia es:" + geografia)
+
+economia="15"
+
+print("el valor de nota de economia es:" + economia)
+
+civica="15"
+print("el valor de nota de civia es:" + civica)
+
+fisica="19"
+print("la nota de fisica es:" + fisica)
+
+pago="100000"
+print("el pago es:" + pago)
+
+promedio_general="17"
+print("el valor de promedio general es:" + promedio_general)
+
+orden_merito="1"
+print("el valor de orden de merito es:" + orden_merito)
+
+registro="00746-18"
+print("el valor de registro de minedu es:" + registro)
+
+primer_nombre="GINO"
+print("el primer nombre es:" + primer_nombre)
+
+segundo_nombre="ROLL"
+print("el segundo nombre es:" + segundo_nombre)
+
+apellido_paterno="BANCES"
+print("el apellido paterno es:" + apellido_paterno)
+
+apellido_materno="VALDERA"
+print("el apellido materno es:" + apellido_materno)
+
+numero_cusos="6"
+print("el valor de numero de cursos es:" + numero_cusos)
+
+mama="mercedes"
+print("el nombre de mama es:" + mama)
+
+apellido_mama="valdera siesquen"
+print("el apellido de mama es:" + apellido_mama)
+
+apellido_paterno_mama="valdera"
+print("el apellido paterno de mama es:" + apellido_paterno_mama)
+
+nombre_de_papa="rigoberto"
+print("el nombre de papa es:" + nombre_de_papa)
+
+apellido_paterno_de_papa="bances"
+print("el apellido paterno de papa es:" + apellido_paterno_de_papa)
+
+apellido_materno_de_papa="inonian"
+print("el apellido materno de papa es:" + apellido_materno_de_papa)
+
+n="saludos"
+print("el valor de n es:" + n)
+
+m="hola a todos"
+print("el valor de m es:" + m)
+
+lengua="castellano"
+print("el idioma de lengua es:" + lengua)
+
+Horas_de_clase=9
+print("El valor de la variable horas de clase es:", Horas_de_clase)
